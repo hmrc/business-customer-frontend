f70824435e6ab11d92223d2e7e74e85002e52ca8
robert.lavender <robert.lavender@digital.hmrc.gov.uk>
2017-11-09 12:20:55 +0000

    ATED-3919: Fix issue with clearing out inputs with switching to no.

--- public/javascript/overseas-company-registration.js
+++ public/javascript/overseas-company-registration.js
@@ -1,24 +1,13 @@
-function showHideIdentifiersSection() {
-    var selectedDiv = $('#hidden-identifiers');
-    var businessUniqueIdField = $('#businessUniqueId');
-    var issuingCountryField = $('#issuingCountry');
-    var issuingCountryHiddenField = $('#issuingCountry_');
-    var issuingInstitutionField = $('#issuingInstitution');
-    selectedDiv.hide();
-
-    if($('#hasBusinessUniqueId-true').is(':checked')) {
-        selectedDiv.show();
-    }
+function clearInputValues() {
 
     $('input[type=radio][name=hasBusinessUniqueId]').change(function(){
         if(this.value == 'true') {
-            selectedDiv.show();
+          // Do nothing
         } else {
-            selectedDiv.hide();
-            businessUniqueIdField.val('');
-            issuingCountryHiddenField.prop('selectedIndex',0);
-            issuingCountryField.val('');
-            issuingInstitutionField.val('');
+            $('#businessUniqueId').val('');
+            $('#issuingCountry_').prop('selectedIndex',0);
+            $('#issuingCountry').val('');
+            $('#issuingInstitution').val('');
         }
     });
 
