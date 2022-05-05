5ec39ab9333d1247d30bf2ac56ed6f99de9397e1
nadinJ <63252254+nadinJ@users.noreply.github.com>
2022-05-05 11:36:27 +0100

    DL-7654: Added titleBuilder

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,21 +6,21 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "5.20.0",
-    "uk.gov.hmrc"       %% "domain"                     % "7.0.0-play-28",
-    "uk.gov.hmrc"       %% "play-partials"              % "8.2.0-play-28",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "1.31.0-play-28",
-    "uk.gov.hmrc"       %% "http-caching-client"        % "9.5.0-play-28",
+    "uk.gov.hmrc"       %% "domain"                     % "8.0.0-play-28",
+    "uk.gov.hmrc"       %% "play-partials"              % "8.3.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "3.15.0-play-28",
+    "uk.gov.hmrc"       %% "http-caching-client"        % "9.6.0-play-28",
     "com.typesafe.play" %% "play-json-joda"             % "2.9.2",
     compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.1" cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib" % "1.7.1" % Provided cross CrossVersion.full
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28"  % "5.20.0"  % "test",
-    "org.jsoup"   %  "jsoup"                   % "1.14.3"  % "test",
-    "org.mockito" %  "mockito-core"            % "4.3.1"   % "test",
-    "org.mockito" %% "mockito-scala"           % "1.17.5" % "test",
-    "org.mockito" %% "mockito-scala-scalatest" % "1.17.5" % "test"
+    "uk.gov.hmrc" %% "bootstrap-test-play-28"  % "5.20.0"   % "test",
+    "org.jsoup"   %  "jsoup"                   % "1.14.3"   % "test",
+    "org.mockito" %  "mockito-core"            % "4.5.1"    % "test",
+    "org.mockito" %% "mockito-scala"           % "1.17.5"   % "test",
+    "org.mockito" %% "mockito-scala-scalatest" % "1.17.5"   % "test"
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
