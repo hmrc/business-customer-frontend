005afe3213b1bdae8afcd2cb31d0325c9639c4a1
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2022-06-30 13:01:49 +0100

    DL-7845 dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "5.24.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "5.25.0",
     "uk.gov.hmrc"       %% "domain"                     % "8.1.0-play-28",
     "uk.gov.hmrc"       %% "play-partials"              % "8.3.0-play-28",
     "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "3.21.0-play-28",
@@ -14,7 +14,7 @@ object AppDependencies {
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28"  % "5.24.0"   % "test",
+    "uk.gov.hmrc" %% "bootstrap-test-play-28"  % "5.25.0"   % "test",
     "org.jsoup"   %  "jsoup"                   % "1.15.1"   % "test",
     "org.mockito" %  "mockito-core"            % "4.6.1"    % "test",
     "org.mockito" %% "mockito-scala"           % "1.17.7"   % "test",
