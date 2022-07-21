485968ae4eadb27298b4991d550182fc4cc15039
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2022-07-21 09:39:57 +0100

    DL-8235 fix country length and updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,14 +8,14 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "5.25.0",
     "uk.gov.hmrc"       %% "domain"                     % "8.1.0-play-28",
     "uk.gov.hmrc"       %% "play-partials"              % "8.3.0-play-28",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "3.21.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "3.22.0-play-28",
     "uk.gov.hmrc"       %% "http-caching-client"        % "9.6.0-play-28",
     "com.typesafe.play" %% "play-json-joda"             % "2.9.2"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-28"  % "5.25.0"   % "test",
-    "org.jsoup"   %  "jsoup"                   % "1.15.1"   % "test",
+    "org.jsoup"   %  "jsoup"                   % "1.15.2"   % "test",
     "org.mockito" %  "mockito-core"            % "4.6.1"    % "test",
     "org.mockito" %% "mockito-scala"           % "1.17.7"   % "test",
     "org.mockito" %% "mockito-scala-scalatest" % "1.17.7"   % "test"
