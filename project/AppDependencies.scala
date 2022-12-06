ce2da6b7eb934534fae2bec94648401e895b88eb
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2022-12-06 16:57:59 +0000

    DL-8857 Dependency and sbt updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,14 +8,14 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "5.25.0",
     "uk.gov.hmrc"       %% "domain"                     % "8.1.0-play-28",
     "uk.gov.hmrc"       %% "play-partials"              % "8.3.0-play-28",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "3.22.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "3.34.0-play-28",
     "uk.gov.hmrc"       %% "http-caching-client"        % "9.6.0-play-28",
-    "com.typesafe.play" %% "play-json-joda"             % "2.9.2"
+    "com.typesafe.play" %% "play-json-joda"             % "2.9.3"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-28"  % "5.25.0"   % "test",
-    "org.jsoup"   %  "jsoup"                   % "1.15.2"   % "test",
+    "org.jsoup"   %  "jsoup"                   % "1.15.3"   % "test",
     "org.mockito" %  "mockito-core"            % "4.7.0"    % "test",
     "org.mockito" %% "mockito-scala"           % "1.17.12"   % "test",
     "org.mockito" %% "mockito-scala-scalatest" % "1.17.12"   % "test"
