228b93a45aa454bea56992660916bb32bbd18dd1
rameshmandanapu1 <155470931+rameshmandanapu1@users.noreply.github.com>
2024-04-29 12:30:15 +0100

    DL-13549-Test library updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,10 +14,7 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % "8.4.0"    % Test,
-    "org.jsoup"         %  "jsoup"                   % "1.17.2"   % Test,
-    "org.mockito"       %  "mockito-core"            % "5.10.0"   % Test,
-    "org.mockito"       %% "mockito-scala"           % "1.17.30"  % Test,
-    "org.mockito"       %% "mockito-scala-scalatest" % "1.17.30"  % Test,
-    "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.18.0" % Test
+    "org.mockito"       %% "mockito-scala-scalatest" % "1.17.31"  % Test,
   )
+  val itDependencies: Seq[ModuleID] = Seq()
 }
