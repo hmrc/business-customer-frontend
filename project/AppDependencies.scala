d46cb2abf862d004db493ac8ecca596ded9cd985
Michael Olafisoye <molafisoye@gmail.com>
2025-01-30 10:46:21 +0000

    DL-15172 upgrade bootstrap-frontend-play-30 to 9.7.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"  %% "bootstrap-frontend-play-30"   % "9.5.0",
+    "uk.gov.hmrc"  %% "bootstrap-frontend-play-30"   % "9.7.0",
     "uk.gov.hmrc"  %% "domain-play-30"               % "10.0.0",
     "uk.gov.hmrc"  %% "play-partials-play-30"        % "10.0.0",
     "uk.gov.hmrc"  %% "play-frontend-hmrc-play-30"   % "11.6.0",
