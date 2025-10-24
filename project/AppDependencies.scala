aeaa0c7e840cbcb6599a6002fb6d7872f19ad66b
Simon Bailey <5601214+thebailers@users.noreply.github.com>
2025-10-24 17:34:57 +0100

    DL-17676: adds numeric inputmode and pattern to utr

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -21,15 +21,15 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"  %% "bootstrap-frontend-play-30"   % "10.1.0",
+    "uk.gov.hmrc"  %% "bootstrap-frontend-play-30"   % "10.3.0",
     "uk.gov.hmrc"  %% "domain-play-30"               % "11.0.0",
-    "uk.gov.hmrc"  %% "play-partials-play-30"        % "10.1.0",
-    "uk.gov.hmrc"  %% "play-frontend-hmrc-play-30"   % "12.8.0",
+    "uk.gov.hmrc"  %% "play-partials-play-30"        % "10.2.0",
+    "uk.gov.hmrc"  %% "play-frontend-hmrc-play-30"   % "12.18.0",
     "uk.gov.hmrc"  %% "http-caching-client-play-30"   % "12.2.0",
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % "10.1.0"    % Test
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % "10.3.0"    % Test
   )
   val itDependencies: Seq[ModuleID] = Seq()
 }
